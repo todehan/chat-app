0d6b8a8e6c75411732b98f3ca7fbde921ac07b25
todehan <todehan@gmail.com>
2019-03-14 20:30:43 +0300

    socket.io redis adapter

--- public/javascripts/controller/chatController.js
+++ public/javascripts/controller/chatController.js
@@ -1,3 +1,4 @@
 app.controller("chatController", ["$scope", ($scope) => {
     const socket = io.connect("http://localhost:3000");
+    
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -6,6 +6,12 @@ const socketApi = {
     io: io
 };
 
+const redisAdapter = require("socket.io-redis");
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
 io.on("connection", socket => {
     console.log("user connected");
 });
